ff9416f3d426992d79a03a0ac3af8c2d7e48313b
DyDyDev <dylan68@sfr.fr>
2022-03-10 22:09:22 +0100

    Initial Commit

---
+++ The Batman/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Batman
+//
+//  Created by Dylan Caetano on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, BATMAN!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Batman/The_BatmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_BatmanApp.swift
+//  The Batman
+//
+//  Created by Dylan Caetano on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_BatmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
