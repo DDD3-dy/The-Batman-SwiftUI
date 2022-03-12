3daa0f3a45b42d83ebacd1d0afb78acb9e0fdbbd
DyDyDev <dylan68@sfr.fr>
2022-03-12 17:18:17 +0100

    CartTheBatman

--- The Batman/ContentView.swift
+++ The Batman/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("Hello, BATMAN!")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Welcome To Gotham")
-                    .foregroundColor(Color.accentColor)
-                    .fontWeight(.bold)
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.accentColor)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Hello, BATMAN!")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .fontWeight(.semibold)
+                        Text("Welcome To Gotham")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.light)
+                    } // VSTACK
+                    Spacer()
+                } // HSTACK
+                .padding(.horizontal, 10)
+                .padding(.top, 10)
+                Spacer()
+                Image("TheBatmanMovieCover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 230, height: 155, alignment: .top)
             } // VSTACK
-        } // HSTACK
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 33)
+        } // ZSTACK
     }
 }
 
@@ -27,3 +53,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
