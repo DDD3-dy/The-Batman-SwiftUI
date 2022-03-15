02497c6c7b36c21e5ea48dce9878a1de812d1d53
DyDyDev <dylan68@sfr.fr>
2022-03-15 22:35:57 +0100

    Data

---
+++ The Batman/Models/Cover.swift
@@ -0,0 +1,13 @@
+//
+//  Cover.swift
+//  The Batman
+//
+//  Created by Dylan Caetano on 15/03/2022.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
