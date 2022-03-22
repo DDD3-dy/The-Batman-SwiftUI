09eb7f9774b22a8b73913c8dab1606b4f5b559b1
DyDyDev <dylan68@sfr.fr>
2022-03-22 20:54:07 +0100

    TabBar

--- The Batman/Views/Components/CellView.swift
+++ The Batman/Views/Components/CellView.swift
@@ -9,25 +9,28 @@ import SwiftUI
 
 struct CellView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Image("TheBatmanMovieCover")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-            Text("The Batman")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-                .padding(-2)
-            Text("Gotham" .uppercased())
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .foregroundColor(.secondary)
-            Text("Bruce Wayne, alias Batman, est un super-héros de fiction appartenant à l'univers de DC Comics.")
-                .multilineTextAlignment(.leading)
-                .lineLimit(2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.secondary)
-        } // VSTACK
+        HStack(alignment: .bottom) {
+            VStack(alignment: .leading) {
+                Image("TheBatmanMovieCover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                Text("The Batman")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .padding(-2)
+                Text("Gotham" .uppercased())
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.secondary)
+                Text("Bruce Wayne, alias Batman, est un super-héros de fiction appartenant à l'univers de DC Comics.")
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.secondary)
+            } // VSTACK
+            
+        } // HSTACK
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
@@ -35,7 +38,7 @@ struct CellView: View {
                         RoundedRectangle(cornerRadius: 30, style: .continuous)
         )
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-        .padding(.horizontal, 35)
+    .padding(.horizontal, 25)
 
     }
 }
